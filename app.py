6218b1e93b2c559ae0f4dbc7ec2f15bd2fde5fe5
rxd321 <68972156+rxd321@users.noreply.github.com>
2022-01-03 01:20:20 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -19,7 +19,9 @@ def main():
         temperature = request.form.get("temperature")
         humidity = request.form.get("humidity")
         windspeed = request.form.get("windspeed")
-
+        print(temperature)
+        print(humidity)
+        print(windspeed)
         # Create DataFrame based on input
         input_variables = pd.DataFrame([[temperature, humidity, windspeed]],
                                        columns=['temperature', 'humidity', 'windspeed'],
@@ -31,11 +33,6 @@ def main():
         prediction = model.predict(input_variables)[0]
     
         # We now pass on the input from the from and the prediction to the index page
-        return "test" #render_template("index.html",
-                                     #original_input={'Temperature':temperature,
-                                      #               'Humidity':humidity,
-                                                     #'Windspeed':windspeed},
-                                     #result=prediction
-                                     #)
+        return render_template("index.html", original_input={'Temperature':temperature,'Humidity':humidity,'Windspeed':windspeed}, result=prediction )
     # If the request method is GET
-    return "error" #render_template("index.html")
+    return render_template("index.html")
